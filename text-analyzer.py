3c40d7e34defa8b130008cb9785ccef1706e85b1
TahashiburRahman <tahashiburrahman5@gmail.com>
2022-09-01 13:06:04 +0600

    First Commit

---
+++ text-analyzer.py
@@ -0,0 +1,48 @@
+#Text Analyzer
+
+text=input("Enter a text of your choice: ")
+letters=[]
+text = text.lower()
+
+letters.append(input("Enter the first letter: ").lower())
+letters.append(input("Enter the second letter: ").lower())
+letters.append(input("Enter the third letter: ").lower())
+
+print("\n")
+print("LETTER REPETITIONS: ")
+
+letter_repetition1 = text.count(letters[0])
+letter_repetition2 = text.count(letters[1])
+letter_repetition3 = text.count(letters[2])
+
+print(f"We have found the letter '{letters[0]}' repeated {letter_repetition1}times.")
+print(f"We have found the letter '{letters[1]}' repeated {letter_repetition2}times.")
+print(f"We have found the letter '{letters[2]}' repeated {letter_repetition3}times.")
+
+print("\n")
+print("NUMBER OF WORDS: ")
+
+word=text.split()
+print(f"We have Found {len(word)} words in your text.")
+
+print("\n")
+print("FIRST AND LAST LETTERS: ")
+
+first_letter = text[0]
+last_letter = text[-1]
+print(f"The initial letter is '{first_letter}', The final letter is '{last_letter}'.")
+
+print("\n")
+print("INVERTED TEXT: ")
+
+word.reverse()
+inverted_text="".join(word)
+print(f"we order your text backwards it will say'{inverted_text}'.")
+
+print("\n")
+print("LOOKING FOR THE WORD PYTHON ")
+
+is_python='python' in text
+Dic_1={True:"was",False:"was not"}
+
+print(f"The word 'Python' {Dic_1[is_python]} found in the text.")
